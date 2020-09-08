ff817f0b7760e7a9f94cc618b05f219e809d1a7c
linsheng <562347359@qq.com>
2020-09-08 19:11:15 +0800

    第一个程序

---
+++ my frist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _myfrist
+
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int p = 1;
+            double s = 1.23;
+            string str = "超人";
+            char c = 'a';
+
+             Console.WriteLine(str);
+             Console.ReadKey();
+        }
+
+    }
+
+}
